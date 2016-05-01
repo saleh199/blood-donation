d347a6004e04ef57216d32b8ede0304ce50b13cd
Saleh Saeed <saleh.saiid@gmail.com>
2016-05-01 13:45:31 +0300

    Add donor schema

---
+++ server/schema/donor.js
@@ -0,0 +1,14 @@
+/**
+ * Created by saleh on 5/1/16.
+ */
+'use strict';
+
+var mongoose = require('mongoose');
+
+var donor = mongoose.model('Donor', {
+    first_name : String,
+    last_name : String,
+    contact_number : String,
+    email : String,
+    blood_group : String
+});
