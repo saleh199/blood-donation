fd75496f1a2f0b2339dc1e4d5b65383fb22d73b5
Saleh Saeed <saleh.saiid@gmail.com>
2016-05-01 14:49:29 +0300

    Addd validation to phone number

--- server/schema/donor.js
+++ server/schema/donor.js
@@ -6,12 +6,31 @@
 var mongoose = require('mongoose');
 
 var donor = mongoose.model('Donor', {
-    first_name : String,
-    last_name : String,
-    contact_number : String,
-    email : String,
-    blood_group : String,
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    contact_number: {
+        type: String,
+        validate: {
+            validator: function(value) {
+                return /(\+|0{2})+([0-9]{2})+(([0-9]+)|\s+([0-9]{3}\s[0-9]{4}\s[0-9]{3}))/g.text(value);
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    blood_group: {
+        type: String,
+        required: true
+    },
     ip: String,
-    longitude : Number,
-    latitude : Number
+    longitude: Number,
+    latitude: Number
 });
