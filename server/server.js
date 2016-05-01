b04d2e830b7248c02a81718ee15f27451e302bbc
Saleh Saeed <saleh.saiid@gmail.com>
2016-05-01 13:54:53 +0300

    Implement mongoose

--- server/server.js
+++ server/server.js
@@ -6,11 +6,23 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var socket = require('socket.io')(server);
+var mongoose = require('mongoose');
+
+/**
+ * DB connection
+ */
+var db_uri = process.env.MONGO_URI || 'mongodb://localhost/blood-donation';
+mongoose.connect(db_uri);
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+    console.log('MongoDB connected to : ' + db_uri);
+});
 
 /**
  * Get port from environment and store in Express.
  */
-
 var port = process.env.PORT || 3000;
 app.set('port', port);
 
